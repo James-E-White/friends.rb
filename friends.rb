e4920b49c5728763176955575e7b368c1fcfe0ca
James White <whitejaad@gmail.com>
2022-07-18 17:22:13 -0700

    Update friends

--- friends.rb
+++ friends.rb
@@ -1,6 +1,10 @@
-name = "Bob"
-if name == "Tess"
+name = "Tess"
+name1 = "Tim"
+
+if name == "Tess" && name1 != "Phil"
   p "This is my best friend #{name}"
+elsif name1 == "Phil"
+p "This is my other good buddy #{name1}."
 else name != "Tess"
   p "This is my other friend #{name}"
 end
