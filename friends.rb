829b171d4a9a8b5570468e2ab024da8bc1e1e441
James White <whitejaad@gmail.com>
2022-07-18 18:33:07 -0700

    Friends1 save

--- friends.rb
+++ friends.rb
@@ -1,10 +1,10 @@
 name = "Tess"
 name1 = "Tim"
 
-if name == "Tess" && name1 != "Phil"
+if name == "Tess" && name1 != "Tim"
   p "This is my best friend #{name}"
-elsif name1 == "Phil"
+elsif name1 == "Tim"
 p "This is my other good buddy #{name1}."
-else name != "Tess"
+else name != "Bob"
   p "This is my other friend #{name}"
 end
